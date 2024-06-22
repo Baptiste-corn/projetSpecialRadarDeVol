b4bfefb6bc18ae657fc29c83035c3336e43f1c66
Baptiste-corn <61588561+Baptiste-corn@users.noreply.github.com>
2024-06-22 18:41:00 -0400

    création classes Avion et Atmosphère

---
+++ Avion.py
@@ -0,0 +1,10 @@
+class Avion:
+    def __init__(self, altitude, ground_speed):
+        self.altitude = altitude
+        self.ground_speed = ground_speed
+        # heading
+        # Consommation
+        #Temps de vol
+        # ...
+
+
