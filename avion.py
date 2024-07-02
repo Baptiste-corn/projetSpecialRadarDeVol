7e013def277f89cc9c375b3a92dd91494693f513
Baptiste-corn <61588561+Baptiste-corn@users.noreply.github.com>
2024-07-02 14:30:40 -0400

    Ajout des attributs masse, surface, envergure, angle_fleche, cd0 et leur get dans avion optimisation et simplification des fonctions de calculs dans controle_vol en passant juste un objet Avion en parametre

--- avion.py
+++ avion.py
@@ -1,16 +1,38 @@
 from atmosphere import Atmosphere
 class Avion:
-    def __init__(self, altitude, ground_speed, atmosphere):
+    def __init__(self, altitude, ground_speed,
+                 atmosphere, envergure, surface, masse, angle_fleche,
+                 cd0
+                 ):
         self.altitude = altitude  # mètres
         self.ground_speed = ground_speed # noeuds
         self.atmosphere = atmosphere
+        self.envergure = envergure
+        self.surface = surface
+        self.masse = masse
+        self.angle_fleche = angle_fleche
+        self.cd0 = cd0
         # heading
         # Consommation
         #Temps de vol
         # ...
 
+    def get_altitude(self):
+        return self.altitude
     def get_ground_speed(self):
         return self.ground_speed
+    def get_envergure(self):
+        return self.envergure
+    def get_surface(self):
+        return self.surface
+    def get_masse(self):
+        return self.masse
+    def get_atmosphere(self):
+        return self.atmosphere
+    def get_angle_fleche(self):
+        return self.angle_fleche
+    def get_cd0(self):
+        return self.cd0
     def affichage(self):
         print(f'Les caractéristiques de l\'avion sont : '
               f'\nAltitude = {self.altitude} mètres'
@@ -27,6 +49,6 @@ class Avion:
 
 
 monAtmopshere = Atmosphere(10000)
-monAvion = Avion(10000, 500, monAtmopshere)
-Avion.affichage(monAvion)
-print(Avion.conditions_atmospheriques(monAvion))
+#monAvion = Avion(10000, 500, monAtmopshere)
+#Avion.affichage(monAvion)
+#print(Avion.conditions_atmospheriques(monAvion))
--- controle_vol.py
+++ controle_vol.py
@@ -6,68 +6,51 @@ from avion import Avion
 import math
 
 
-def controle(avion, new_altitude, new_vitesse, atmosphere):
-
-    atmosphere = Atmosphere(new_altitude)
-    avion = Avion(new_altitude, new_vitesse, atmosphere)
-
-    print("nouvelles conditions")
-    avion.affichage()
-    print(avion.conditions_atmospheriques())
-
-    return avion
-
-def portance (densite, vitesse_sol, surface, envergure):
-    # Paramètres de départs
-    a0 = 2 * math.pi  # 1/rad
-    alpha_l0 = 0  # angle d'attaque à portance nulle
-    alpha_aile = 3  # angle d'attaque de l'avion en croisière
-
-    allongement = envergure**2 / surface
-    a = a0 / ( 1 + 2 / (allongement))
-    coef_portance = a * (alpha_aile - alpha_l0) * (math.pi / 180) #  coef portance en reconvertissant a en degré
-    portance  = 0.5 * densite * vitesse_sol**2 * surface * coef_portance
-
-    return portance
-
-def portance_L (poids):
-    return poids * 9.81  # Vol en palier
+def portance_L (monAvion: Avion):
+    return monAvion.get_masse() * 9.81  # Vol en palier
 
 def coef_portance_cl (cl_alpha):
     alpha_l0 = 0  # angle d'attaque à portance nulle
     alpha_aile = 3  # angle d'attaque de l'avion en croisière
     return cl_alpha * (alpha_aile - alpha_l0)
 
-def cl_avec_portance (L, densite, vitesse, surface):
-    return L / (0.5 * densite * surface * vitesse**2)
+def cl_avec_portance (monAvion: Avion):
+    return portance_L(monAvion) / (0.5 * monAtmo.get_density() * monA320.get_surface() * monA320.get_ground_speed()**2)
 
-def coef_trainee (envergure, surface, angle_aile_bord_attaque, cd0, cl):
-    def allongement_AR(envergure, surface):
-        return envergure ** 2 / surface
+def allongement_AR_2(monAvion: Avion):
+    return monAvion.get_envergure()**2 / monAvion.get_surface()
 
-    def oswald_factor_e (envergure, surface, angle_aile_bord_attaque):
-        if angle_aile_bord_attaque > 30:
-            return 4.61 * (1 - 0.045 * allongement_AR(envergure, surface) ** 0.68 ) * (math.cos(angle_aile_bord_attaque))**0.15 - 3.1
-        else:
-            return 0.8
+def oswald_factor_e_2(monAvion: Avion):
+    if monAvion.get_angle_fleche() > 30:
+        return 4.61 * (1 - 0.045 * allongement_AR_2(monAvion) ** 0.68) * (math.cos(monAvion.get_angle_fleche())) ** 0.15 - 3.1
+    else:
+        return 0.8
 
-    def correction_k(envergure, surface, angle_aile_bord_attaque):
+def correction_k_2(monAvion: Avion):
+    return 1 / (math.pi * oswald_factor_e_2(monAvion) * allongement_AR_2(monAvion))
 
-        return 1 / (math.pi * oswald_factor_e(envergure, surface, angle_aile_bord_attaque) * allongement_AR(envergure, surface))
+def coef_trainee_2(monAvion: Avion):
+    return monAvion.get_cd0() + correction_k_2(monAvion) * cl_avec_portance(monAvion)
+def trainee(monAvion: Avion):
+    return 0.5 * monAtmo.get_density() * monAvion.get_ground_speed() **2 * monAvion.get_surface() * coef_trainee_2(monAvion)
 
-    print(allongement_AR(envergure, surface), oswald_factor_e (envergure, surface, angle_aile_bord_attaque), correction_k(envergure, surface, angle_aile_bord_attaque))
-    return cd0 + correction_k(envergure, surface, angle_aile_bord_attaque) * cl**2
+def finesse(monAvion : Avion):
+    return cl_avec_portance(monAvion) / coef_trainee_2(monAvion)
 
 
 monAtmo = Atmosphere(10000)
-monAvion = Avion(10000, 283, monAtmo)
-controle(monAvion, 5000, 250, monAtmo)
-print(monAtmo.get_density())
-print(portance(monAtmo.get_density(), 405, 112.4, 34.1))
-print(portance_L(73500))
+monA320 = Avion(10000, 238, monAtmo, 34.1, 112.4, 73500, 25, 0.02)
+lift = portance_L(monA320)
+cl = cl_avec_portance(monA320)
+drag = trainee(monA320)
+print(f'L = {lift} N, CL = {cl}, D = {drag} N')
+print(finesse(monA320))
 
-monA320 = Avion(10000, 238, monAtmo)
-lift = portance_L(73500)
-cl = cl_avec_portance(lift, monAtmo.get_density(), monA320.get_ground_speed(), 112.4)
-trainee_A320 = 0.5 * monAtmo.get_density() * monA320.get_ground_speed() ** 2 * coef_trainee(34.1, 112.4, 30, 0.02, cl) * 112.4
-print(f'L = {lift} N, CL = {cl}, D = {trainee_A320} N')
+# A320 W=73500kg S=112.4m2 b=34.1m delta_le=25° cd0=0.02
+# A330 W=242000kg S=361.6m2 b=60.3m delta_le=30° cd0=0.025
+# A380 W=560000kg S=845m2 b=79.8m delta_le=33.5° cd0=0.022
+# B777 W=299000kg S=427.8m2 b=60.9m delta_le=31.6° cd0=0.025
+# B737 W=79000kg S=124.6m2 b=35.8m delta_le=25° cd0=0.02
+# B787 W=227930kg S=325m2 b=60.1m delta_le=32.2° cd0=0.017
+# B747 W=396890kg S=511m2 b=64.4m delta_le=37.5° cd0=0.024
+# B727 W=95000kg S=164.3m2 b=32.9m delta_le=25° cd0=0.03
